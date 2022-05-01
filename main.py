2e8a2103cc3c08a01866bebce94b26ea0d81434d
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-05-01 16:56:49 +0300

    download book covers

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import dotenv
 from pathlib import Path
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin
+from urllib.parse import urlparse
+
+
+def get_file_ext(url_img):
+    cut = urlparse(url_img)
+    return os.path.splitext(cut.path)[-1]
 
 
 def find_url():
@@ -13,8 +20,9 @@ def find_url():
         try:
             response.raise_for_status()
             check_for_redirect(response)
-            title = find_tag(response)
+            title, url_img = find_tag(response)
             download_txt(title, id)
+            download_image(url_img, title)
         except:
             print(f'Книги не найдено! Пропускаем!')
             continue
@@ -26,8 +34,10 @@ def find_tag(response):
     title_text = title_tag.text
     title_split = title_text.split("::")
     title = title_split[0].strip(" \xa0 ")
+    book_img_id = soup.find(class_='bookimage').find('img')['src']
+    url_img = (urljoin('https://tululu.org/', book_img_id))
     print(title)
-    return title
+    return title, url_img
 
 
 def download_txt(title, id, folder='books/'):
@@ -40,6 +50,15 @@ def download_txt(title, id, folder='books/'):
         file.write(response.content)
 
 
+def download_image(url_img, title, folder='image/'):
+    response = requests.get(url_img)
+    response.raise_for_status()
+    file_ext = get_file_ext(url_img)
+    filepath = os.path.join(folder, sanitize_filename(title) + file_ext)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
@@ -47,6 +66,7 @@ def check_for_redirect(response):
 
 def main():
     Path("books").mkdir(parents=True, exist_ok=True)
+    Path("image").mkdir(parents=True, exist_ok=True)
     dotenv.load_dotenv()
     find_url()
 
