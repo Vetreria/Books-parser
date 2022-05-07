e79a946faee10d2e3f6c69d269aa23569b4d7cd3
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-05-07 21:06:57 +0300

    Полный функционал

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-import requests
-from pathlib import Path
-from bs4 import BeautifulSoup
-from pathvalidate import sanitize_filename
+from pprint import pprint
 from urllib.parse import urljoin
 from urllib.parse import urlparse
-from pprint import pprint
+from pathlib import Path
 import argparse
+import os
 
 
-def find_book(books_tag, start_id, end_id):
-    for id in range(start_id, end_id + 1):
+import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+from tqdm import tqdm
+
+
+def get_book(books_tag, start_end):
+    start_id, end_id = start_end
+    for id in tqdm(range(start_id, end_id + 1), desc="Собираем книжки"):
         try:
             url = f"https://tululu.org/b{id}/"
             response = requests.get(url)
             response.raise_for_status()
             check_for_redirect(response)
-            title, url_img, genres, comments, author = parse_book_page(
-                response)
-            books_tag[id] = {
-                'Название': title,
-                'Автор': author,
-                'Картинка': url_img,
-                'Жанр': genres,
-                'Отзывы': comments
-            }
+            books_tag[id] = parse_book_page(
+                response)  
         except:
-            print(f'Книгa {id} не найденa!')
             continue
     return books_tag
 
@@ -38,12 +35,42 @@ def parse_book_page(response):
     url_img = (urljoin('https://tululu.org/', book_img_id))
     genres = [genre.text for genre in soup.select('span.d_book a')]
     comments = [comment.text for comment in soup.select('.texts span')]
-    return title, url_img, genres, comments, author
+    return {
+                'Название': title,
+                'Автор': author,
+                'Картинка': url_img,
+                'Жанр': genres,
+                'Отзывы': comments
+            }
 
 
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
+
+
+def get_file_ext(url_img):
+    cut = urlparse(url_img)
+    return os.path.splitext(cut.path)[-1]
+
+
+def download_txt(title, id, folder='books/'):
+    url = f"https://tululu.org/txt.php?id={id}/"
+    response = requests.get(url)
+    response.raise_for_status()
+    filename = f"{id}.{title}.txt"
+    filepath = os.path.join(folder, sanitize_filename(filename) + '.txt')
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+
+def download_image(url_img, title, folder='image/'):
+    response = requests.get(url_img)
+    response.raise_for_status()
+    file_ext = get_file_ext(url_img)
+    filepath = os.path.join(folder, sanitize_filename(title) + file_ext)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
 
 
 def create_parser():
@@ -52,16 +79,27 @@ def create_parser():
                         help='С какого ID парсить', type=int)
     parser.add_argument('end', nargs='?',  default=11,
                         help='По какой ID парсить', type=int)
+    parser.add_argument('-i', '--get_imgs', action='store_true', default=False, help='Cкачивать обложки книг')
+    parser.add_argument('-t', '--get_txt', action='store_true', default=False, help='Cкачивать текст книг')
     return parser
 
 
 def main():
+    Path("books").mkdir(parents=True, exist_ok=True)
+    Path("image").mkdir(parents=True, exist_ok=True)
     parser = create_parser()
     namespace = parser.parse_args()
-    start_id, end_id = namespace.start, namespace.end
+    start_end = (namespace.start, namespace.end)
     books_tag = {}
-    find_book(books_tag, start_id, end_id)
+    get_book(books_tag, start_end)
     pprint(books_tag)
+    if namespace.get_imgs:
+        for id in tqdm(books_tag.keys(), desc="Скачиваем обложки"):
+            download_image(books_tag[id]['Картинка'], books_tag[id]['Название'])
+    if namespace.get_txt:
+        for id in tqdm(books_tag.keys(), desc="Скачиваем книжки"):
+            download_txt(books_tag[id]['Название'], id)
+
 
 
 if __name__ == "__main__":
