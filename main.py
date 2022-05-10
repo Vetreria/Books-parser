e56fc06e9716d03686fd291c94efedc6a2fdd7de
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-05-10 16:08:08 +0300

    Исправления

--- main.py
+++ main.py
@@ -13,18 +13,22 @@ from tqdm import tqdm
 logger = logging.getLogger("logger")
 
 
-def get_books(start_end_img_txt):
+def get_books(start_id, end_id, get_imgs, get_txt):
     books_tag = {}
-    start_id, end_id, get_imgs, get_txt = start_end_img_txt
-    for id in tqdm(range(start_id, end_id + 1), desc="Собираем книжки"):
+    for book_id in tqdm(range(start_id, end_id + 1), desc="Собираем книжки"):
         while True:
             try:
-                url = f"https://tululu.org/b{id}/"
+                url = f"https://tululu.org/b{book_id}/"
                 response = requests.get(url)
                 response.raise_for_status()
                 check_for_redirect(response)
-                books_tag[id] = parse_book_page(
-                    response, get_imgs, get_txt, id)
+                books_tag[book_id] = parse_book_page(
+                    response, url)
+                if get_imgs:
+                    download_image(
+                        books_tag[book_id]['Image'], books_tag[book_id]['Title'])
+                if get_txt:
+                    download_txt(books_tag[book_id]['Title'], book_id)
                 break
             except requests.HTTPError:
                 logger.warning('Книга не найдена')
@@ -35,22 +39,18 @@ def get_books(start_end_img_txt):
     return books_tag
 
 
-def parse_book_page(response, get_imgs, get_txt, id):
+def parse_book_page(response, url):
     soup = BeautifulSoup(response.text, 'lxml')
     title = soup.find('h1').text.split("::")[0].strip(" \xa0 ")
     author = soup.find('h1').text.split("::")[1].strip(" \xa0 ")
-    book_img_id = soup.find(class_='bookimage').find('img')['src']
-    url_img = (urljoin('https://tululu.org/', book_img_id))
+    book_img = soup.find(class_='bookimage').find('img')['src']
+    img_url = (urljoin(url, book_img))
     genres = [genre.text for genre in soup.select('span.d_book a')]
     comments = [comment.text for comment in soup.select('.texts span')]
-    if get_imgs:
-        download_image(url_img, title)
-    if get_txt:
-        download_txt(title, id)
     return {
         'Title': title,
         'Author': author,
-        'Image': url_img,
+        'Image': img_url,
         'Genres': genres,
         'Comments': comments
     }
@@ -62,26 +62,28 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def get_file_ext(url_img):
-    cut = urlparse(url_img)
-    return os.path.splitext(cut.path)[-1]
+def get_file_ext(img_url):
+    split_url = urlparse(img_url)
+    return os.path.splitext(split_url.path)[-1]
 
 
-def download_txt(title, id, folder='books/'):
+def download_txt(title, book_id, folder='books/'):
     url = f"https://tululu.org/txt.php"
-    params = {'id': id}
+    params = {'id': book_id}
     response = requests.get(url, params=params)
     response.raise_for_status()
-    filename = f"{id}.{title}.txt"
+    check_for_redirect(response)
+    filename = f"{book_id}.{title}.txt"
     filepath = os.path.join(folder, sanitize_filename(filename) + '.txt')
     with open(filepath, 'wb') as file:
         file.write(response.content)
 
 
-def download_image(url_img, title, folder='image/'):
-    response = requests.get(url_img)
+def download_image(img_url, title, folder='image/'):
+    response = requests.get(img_url)
     response.raise_for_status()
-    file_ext = get_file_ext(url_img)
+    check_for_redirect(response)
+    file_ext = get_file_ext(img_url)
     filepath = os.path.join(folder, sanitize_filename(title) + file_ext)
     with open(filepath, 'wb') as file:
         file.write(response.content)
@@ -113,10 +115,9 @@ def main():
 
     parser = create_parser()
     namespace = parser.parse_args()
-    start_end_img_txt = (namespace.start, namespace.end,
-                         namespace.get_imgs, namespace.get_txt)
-
-    books_tag = get_books(start_end_img_txt)
+    start_id, end_id, get_imgs, get_txt = (namespace.start, namespace.end,
+                                           namespace.get_imgs, namespace.get_txt)
+    books_tag = get_books(start_id, end_id, get_imgs, get_txt)
     logger.warning(books_tag)
 
 
