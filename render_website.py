2f0a4166d979afca33e73498b9b2b102dee1c70d
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-06-05 16:08:04 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -19,6 +19,7 @@ def render_page():
         loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
     )
     books_in_pages = list(chunked(category, count_per_page))
+    count_page = len(books_in_pages)
 
     for page_num, books_catalog in enumerate(books_in_pages, 1):
         books_catalog = [books_card[book_card] for books_card in books_catalog for book_card in books_card]
@@ -26,7 +27,7 @@ def render_page():
         row_catalog = chunked(books_catalog, 2)
         template = env.get_template("template.html")
         rendered_page = template.render(
-            category=row_catalog)
+            category=row_catalog, page_num=page_num, count_page=count_page)
         with open(f"pages/index{page_num}.html", "w", encoding="utf8") as file:
             file.write(rendered_page)
     
