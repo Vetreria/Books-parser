5c99efd3898a14f6bb411d76d215075ced35a1a0
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-06-05 14:15:02 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,6 +1,8 @@
 import json
 import os
+import os.path
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from pathlib import Path
 
 import dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -9,26 +11,33 @@ from more_itertools import chunked
 
 
 def render_page():
+    count_per_page = 10
     with open("content/category.json", "r", encoding="utf8") as file:
         category_json = file.read()
     category = json.loads(category_json)
     env = Environment(
         loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
     )
-    books_catalog = [books_card[book_card] for books_card in category for book_card in books_card]
-    # print(books_catalog)
-    row_catalog = chunked(books_catalog, 2)
-    template = env.get_template("template.html")
-    rendered_page = template.render(
-        category=row_catalog)
-    with open("index.html", "w", encoding="utf8") as file:
-        file.write(rendered_page)
+    books_in_pages = list(chunked(category, count_per_page))
+
+    for page_num, books_catalog in enumerate(books_in_pages, 1):
+        books_catalog = [books_card[book_card] for books_card in books_catalog for book_card in books_card]
+        # print(books_catalog)
+        row_catalog = chunked(books_catalog, 2)
+        template = env.get_template("template.html")
+        rendered_page = template.render(
+            category=row_catalog)
+        with open(f"pages/index{page_num}.html", "w", encoding="utf8") as file:
+            file.write(rendered_page)
     
 
    
 def main():
-    render_page()
+    
+    
     dotenv.load_dotenv()
+    Path("pages").mkdir(parents=True, exist_ok=True)
+    # render_page()
     server = Server()
     server.watch('template.html', render_page)
     server.serve(port=5500, root='.')
